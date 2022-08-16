9ea29e6aa4f6b45db32df11a984ece529b9a5a40
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-16 23:53:11 +0300

    fix: изменить порядок массива всех карточек на сервере (сначала новые)

--- backend/controllers/cards/getAllCards.js
+++ backend/controllers/cards/getAllCards.js
@@ -3,7 +3,7 @@ const { Card } = require('../../models/card');
 async function getAllCards(req, res, next) {
   try {
     const cards = await Card.find({}).populate('owner').populate('likes');
-    res.send(cards);
+    res.send(cards.reverse());
   } catch (err) {
     next(err);
   }
